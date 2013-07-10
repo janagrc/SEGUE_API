7ba6b48c79615e5b972f12e412559258cad2e322
Kristof Bognar <kb1578@nyu.edu>
2013-07-10 16:12:46 -0400

    added openfile function

--- spectrum.py
+++ spectrum.py
@@ -1,8 +1,19 @@
-class DataFile(object):
-    def openfile(self,filepath=None):
-        from astropy.io import fits
-        self.datafile = fits.open(self.filepath, memmap=True)
+# class DataFile(object):
+#     def openfile(self,filepath=None):
+#         from astropy.io import fits
+#         self.datafile = fits.open(self.filepath, memmap=True)
 
 class Star(object):
+
+
+def openfile(filename):
+    
+    from astropy.io import fits
+    data = fits.open("filename")
+
+    return "The fits file is opened as 'data'"
+
+
+
 
  
