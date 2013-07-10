482d929f5ea15d181bf97f600afe29d6bf449989
Kristof Bognar <kb1578@nyu.edu>
2013-07-10 16:32:54 -0400

    update

--- spectrum.py
+++ spectrum.py
@@ -9,9 +9,16 @@ class Star(object):
 def openfile(filename):
     
     from astropy.io import fits
-    data = fits.open("filename")
+    datafile = fits.open("filename")
+    
+    logg = datafile[1].data.field("LOGG_ADOP")
+    feh = datafile[1].data.field("FEH_ADOP")
+    teff = datafile[1].data.field("TEFF_ADOP")
 
-    print "The fits file is opened as 'data'"
+        
+    print "The fits file is opened as 'datafile'"
+
+
 
 
 
