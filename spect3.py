75fccb52fa8e0fb0fb67f63f96b2d96139091138
SHLC <statia@berkeley.edu>
2013-07-11 19:19:36 -0400

    spect3.py changes

--- spect3.py
+++ spect3.py
@@ -5,7 +5,7 @@ import pylab
 import math
 
 class Star(object):
-	def__init__(self):
+	def __init__(self):
 		self.fields = {}
 	
 #class DataFile(object,requiredfields=None):
@@ -15,25 +15,29 @@ class DataFile(object):
 		datafile = fits.open(path)
 		column_names = datafile[1].columns.names
 		self.stars = [] #list of star objects, each with a dict of fields
-		#for row in xrange(0,datafile[0].header['naxis2']
 		#for row in xrange(0,len(datafile[1].data.field('TEFF_ADOP')+1):
-		while x is True:
-		    try:
-		        stardata=datafile[1].data[row]
-		        if goodStar(stardata):
-		            s=Star()
-		            s.fields[column]=stardata.field(column)
-		            self.stars.append(s)
-		    except IndexError:
-		        x=False
+		x = True
+		row=0
+		print x
+		#while x is True:
+		#    try:
+		#        stardata=datafile[1].data[row]
+		#        if goodStar(stardata):
+		        #    s=Star()
+		        #    s.fields[column]=stardata.field(column)
+		        #    self.stars.append(s)
+		        #    row=row+1
+		#    except IndexError:
+		#        x=False
 		        
 def goodStar(stardata, badValue=-9999):
     goodvalues=True
-    if stardata["LOGG_ADOP"]=badValue or stardata["FEH_ADOP"]=badValue or stardata["TEFF_ADOP"]:
+    if (stardata["LOGG_ADOP"] == badValue) or (stardata["FEH_ADOP"] == badValue) or (stardata["TEFF_ADOP"]==badvalue):
         goodvalues=False
     return goodvalues
     
     
 #### Good to here    
     
- 
+if __name__ == "__main__":
+	df = DataFile("../data/ssppOut-dr9.fits")
