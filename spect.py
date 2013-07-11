831de46bab3fd02faf74c0f15d7a074c86e6cfde
SHLC <statia@berkeley.edu>
2013-07-11 09:22:57 -0400

    changed naming conventions somewhat

--- spect.py
+++ spect.py
@@ -5,14 +5,11 @@ import pylab
 import math
 
 
-class datafile:
-	#def __init__(self, path, variableList=["LOGG_ADOP", "FEH_ADOP", "TEFF_ADOP", "SPECTYPE_HAMMER"]):
+class DataFile:
 	def __init__(self, path, variableList=["LOGG_ADOP", "FEH_ADOP", "TEFF_ADOP", "SPECTYPE_HAMMER"]):
+	#Input a check for the path?
 		self.path = path
 		datafile = fits.open(path)
-		#self.dataDict = {}
-		#for variable in variableList:
-		#	self.dataDict[variable] = np.array(datafile[1].data.field(variable))
 		self.logg = np.array(datafile[1].data.field("LOGG_ADOP"))
 		self.feh = np.array(datafile[1].data.field("FEH_ADOP"))
 		self.teff = np.array(datafile[1].data.field("TEFF_ADOP"))
@@ -20,23 +17,20 @@ class datafile:
 		self.clean()
 		
 	def clean(self):
-		self.logg, self.feh, self.teff, self.spectypehammer = cleanData([self.logg, self.feh, self.teff, self.spectypehammer])
-		#cleanedDataList = cleanData(self.dataDict.values())
+		self.logg, self.feh, self.teff, self.spectypehammer = cleandata([self.logg, self.feh, self.teff, self.spectypehammer])
 		
-	def uniqueSpectralTypes(self):
+	def unique_spectral_types(self):
 		return list(set(self.spectypehammer))
 		
-	def plotHist(self):
+	def plothist(self):
 		fig,axes = pylab.subplots(1, 3)
 		axes[0].hist(self.feh, label="[Fe/H]", facecolor="red", alpha=0.5, histtype='stepfilled', bins=20)
 		axes[1].hist(self.feh, label="[Fe/H]", facecolor="red", alpha=0.5, histtype='stepfilled')
 		axes[2].hist(self.feh, label="[Fe/H]", facecolor="red", alpha=0.5, histtype='stepfilled')
-		#axes[1].hist(self.PUTRADIALVELOCITYARRAYHERE, label="[Fe/H]", facecolor="red", alpha=0.5, histtype='stepfilled')
-		#axes[2].hist(self.PUTHELIODISTARRAYHERE, label="[Fe/H]", facecolor="red", alpha=0.5, histtype='stepfilled')
 		pylab.show()
 	
 
-def cleanData(arrayList, badValue=-9999):
+def cleandata(arrayList, badValue=-9999):
 	goodIndices = []
 	for i in xrange(len(arrayList[0])):
 		foundBadValue = False
@@ -53,22 +47,22 @@ def cleanData(arrayList, badValue=-9999):
 	
 	return arrayList
 
-def valuesEq(x,y):
+def values_eq(x,y):
 	for i in xrange(len(x)):
 		for j in xrange(len(x[0])):
 			if x[i][j] != y[i][j]: return False
 	return True
 		
-def testClean():
+def test_clean():
 	a = np.array([1,2,5,4,-9999,4,1,2])
 	b = np.array([1,2,9,4,-9999,4,-9999,2])
 	c = np.array([2,-9999,5,4,-2,8,1,2])
 	d = np.array([1,5,5,4,-2,4,1,6])
 	#print "clean([a,b,c,d])",  clean([a,b,c,d])
-	assert valuesEq(clean([a,b,c,d]), [[1,5,4,4,2], [1,9,4,4,2], [2,5,4,8,2], [1,5,4,4,6]])
+	assert values_eq(clean([a,b,c,d]), [[1,5,4,4,2], [1,9,4,4,2], [2,5,4,8,2], [1,5,4,4,6]])
 		
 
 if __name__ == "__main__":
-	df = datafile("/Users/janagrc/desktop/ssppOut-dr9.fits")
-	print "Spectral types: ", df.uniqueSpectralTypes()
-	df.plotHist()
+	df = DataFile("/Users/janagrc/desktop/ssppOut-dr9.fits")
+	print "Spectral types: ", df.unique_spectral_types()
+	df.plothist()
