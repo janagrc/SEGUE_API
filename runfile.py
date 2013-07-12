d1137f5bffb54d2dd0fa403e340adb30ee4795ec
SHLC <statia@berkeley.edu>
2013-07-11 22:10:58 -0400

    working version of DataFile (but really really slow, only reading 10000 lines right now)

---
+++ runfile.py
@@ -0,0 +1,37 @@
+import numpy as np
+import copy
+import pylab
+import math
+import matplotlib.pyplot as plt
+from spect3 import DataFile
+
+#How do you reimport after making changes?
+x = DataFile("../data/ssppOut-dr9.fits")
+# Print out unique spectral types. Note: I had to loop through stars to get this...
+print x.spectypes
+
+# Either I'm missing something or I really don't see the benefit of putting each star
+# in its own object. Now I have to loop through stars to get out a single column
+feh=[]
+rv=[]
+dist=[]
+for i in xrange(0, len(x.stars)):
+    feh.append(x.stars[i].fields["FEH_ADOP"])
+    rv.append(x.stars[i].fields["RV_ADOP"])
+    dist.append(x.stars[i].fields["DIST_ADOP"])
+    
+# I can't plot anything!
+def plothist(feh,rv,dist)
+    fig,axes = pylab.subplots(1, 3)
+    axes[0].hist(feh, label="[Fe/H]", facecolor="red", alpha=0.5, histtype='stepfilled', bins=20)
+    axes[1].hist(rv, label="RV", facecolor="red", alpha=0.5, histtype='stepfilled')
+    axes[2].hist(dist, label="Dist", facecolor="red", alpha=0.5, histtype='stepfilled')
+    pylab.show()
+
+plothist(feh,rv,dist)
+# Function to accept galactic longitude, latitude and heliocentric distance and convert to Galactic X, Y, Z
+# plothist(feh,rv,galdist)
+# Add column to stars that has Galactocentric distance?
+
+# Select stars with [Fe/H]<-1
+# plothist...
--- spect3.py
+++ spect3.py
@@ -5,68 +5,47 @@ import pylab
 import math
 
 class Star(object):
+# Define a star object, which is an (empty) dictionary of fields
 	def __init__(self):
 		self.fields = {}
-=======
-# class Star(object):
-# 	def__init__(self):
-# 		self.fields = {}
->>>>>>> 6e3143fabfbbb6cf2652eac9e3de0dbf9cc092b0
-	
-#class DataFile(object,requiredfields=None):
+
 class DataFile(object):
-	def __init__(self, path):
-		self.path = path
-		datafile = fits.open(path)
-		column_names = datafile[1].columns.names
-		self.stars = [] #list of star objects, each with a dict of fields
-		#for row in xrange(0,len(datafile[1].data.field('TEFF_ADOP')+1):
-<<<<<<< HEAD
-		x = True
-		row=0
-		print x
-		#while x is True:
-		#    try:
-		#        stardata=datafile[1].data[row]
-		#        if goodStar(stardata):
-		        #    s=Star()
-		        #    s.fields[column]=stardata.field(column)
-		        #    self.stars.append(s)
-		        #    row=row+1
-		#    except IndexError:
-		#        x=False
-		        
-def goodStar(stardata, badValue=-9999):
-    goodvalues=True
-    if (stardata["LOGG_ADOP"] == badValue) or (stardata["FEH_ADOP"] == badValue) or (stardata["TEFF_ADOP"]==badvalue):
-=======
-		x  = True
-		row = 0
-		while x is True:
-		    try:
-		        stardata=datafile[1].data[row]
-		        # if goodStar(stardata):
-		        #     s=Star()
-		        #     s.fields[column]=stardata.field(column)
-		        #     self.stars.append(s)
-			row += 1
-			print row
-		    except IndexError:
-		        x=False
-		        
-def goodStar(stardata, badValue=-9999):
-    goodvalues=True
-    if stardata["LOGG_ADOP"]==badValue or stardata["FEH_ADOP"]==badValue or stardata["TEFF_ADOP"]==badValue:
->>>>>>> 6e3143fabfbbb6cf2652eac9e3de0dbf9cc092b0
-        goodvalues=False
-    return goodvalues
-    
-    
-#### Good to here    
-    
-if __name__ == "__main__":
-	df = DataFile("../data/ssppOut-dr9.fits")
-=======
+    def __init__(self, path):
+        self.path = path
+        datafile = fits.open(path)
+        self.column_names = datafile[1].columns.names
+		# Datafile.stars is a list of star objects
+        self.stars = [] 
+        # Loop through the file, reading each row one at a time, 
+        # until reaching the end of the file
+        eof = False
+        row = 0
+#        while eof is False:
+# Takes too long to read in. I have to set a limit or it takes like an hour to finish
+        while eof == False and row < 10000 :
+            try:
+                stardata=datafile[1].data[row]
+                if goodStar(stardata):
+                    s=Star()
+                    for column in self.column_names:
+                        s.fields[column]=stardata.field(column)
+                    self.stars.append(s)
+                print row
+                row = row+1
+            except IndexError:
+                eof = True
+                print "eof"		 
+        stypes=[]
+        for i in xrange(0, len(self.stars)):
+            stypes.append(self.stars[i].fields["SPECTYPE_HAMMER"])
+        self.spectypes=unique_spectral_types(stypes)
+       
+   	def goodStar(stardata, badValue=-9999):
+        goodvalues=True
+        if stardata["LOGG_ADOP"]==badValue or stardata["FEH_ADOP"]==badValue or stardata["TEFF_ADOP"]==badValue:
+            goodvalues=False
+        return goodvalues
+
+    def unique_spectral_types(stypes):
+		return list(set(stypes))
  
-x = DataFile("/home/kiki/scicoder-workshop/ssppOut-dr9.fits")
->>>>>>> 6e3143fabfbbb6cf2652eac9e3de0dbf9cc092b0
